ae19c751ac068626ccc8fe8df6aa90ee500c5184
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 18:53:36 +0700

    [UPDATE] Web / remove unused function, state

--- WEB/components/SubjectList.js
+++ WEB/components/SubjectList.js
@@ -6,27 +6,35 @@ export default class extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            subjects: []
+            subjects: [],
+            message: 'no course to be show or please wait'
         }
         this.getList = this.getList.bind(this)
+        this.setMessage = this.setMessage.bind(this)
     }
     async getList() {
         const data = await axios.get(`subject/${this.props.user.id}`, {
-            baseURL: 'http://localhost:8090'
+            baseURL: 'https://subjectservice-restless-mongoose.mybluemix.net'
         })
-        console.log(data.data)
         this.setState({
             subjects: data.data
         })
+
     }
     componentDidMount() {
         this.getList()
     }
+    setMessage(message){
+        this.setState({
+            message: message
+        })
+    }
+    
 
     render() {
         const subjects = this.state.subjects.map(subject => {
             return (
-                <Subject data={subject} key={subject.id} id={this.props.user.id} year={this.props.user.year} />
+                <Subject data={subject} key={subject.id} id={this.props.user.id} year={this.props.user.year} setMessage={this.setMessage}  />
             )
         })
         return (
@@ -40,6 +48,7 @@ export default class extends React.Component {
 
                 </div>
                 {subjects}
+                <h2>{this.state.message}</h2>
             </div>
             
         )
