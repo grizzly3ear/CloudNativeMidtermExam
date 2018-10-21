0525a5b1ac109fd40780c751f1746c14646096c7
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 18:44:10 +0700

    [REMOVE] Enroll / unused java class

--- EnrollService/src/main/java/com/sit/registration/EnrollService/Enroll/EnrollAdaptor.java
+++
@@ -1,9 +0,0 @@
-package com.sit.registration.EnrollService.Enroll;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-@Service
-public class EnrollAdaptor {
-    
-}
