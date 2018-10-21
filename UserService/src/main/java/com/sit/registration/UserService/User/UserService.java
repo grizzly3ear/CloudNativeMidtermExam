1c57c396975dfd7854474fc506f90499eb7a3634
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 17:33:20 +0700

    [UPDATE] User / user can login

--- UserService/src/main/java/com/sit/registration/UserService/User/UserService.java
+++ UserService/src/main/java/com/sit/registration/UserService/User/UserService.java
@@ -5,17 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService{
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-    
-    public List<User> getAll(){
+
+    public List<User> getAll() {
         return userRepository.findAll();
     }
 
-    public long getYear(long studentId){
+    public long getYear(long studentId) {
         return userRepository.getOne(studentId).getYear();
     }
 
+    public User login(User user) {
+        User userAuthen = userRepository.getOne(user.getId());
+        if (attempt(user)) {
+            return userAuthen;
+        }
+
+        return null;
+    }
+
+    public boolean attempt(User user) {
+        String databasePassword = userRepository.getOne(user.getId()).getPassword();
+        String password = user.getPassword();
+        return databasePassword.equals(password);
+    }
 }
