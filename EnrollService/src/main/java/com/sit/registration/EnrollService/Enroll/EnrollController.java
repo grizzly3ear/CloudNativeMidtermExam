74fff9d1419083f783d2b74c4dec669fed299f4f
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 17:34:29 +0700

    [UPDATE] Enroll / fix CORS

--- EnrollService/src/main/java/com/sit/registration/EnrollService/Enroll/EnrollController.java
+++ EnrollService/src/main/java/com/sit/registration/EnrollService/Enroll/EnrollController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +14,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="*", maxAge=3600)
 public class EnrollController {
     
     @Autowired
     private EnrollService enrollService;
 
     @PostMapping("/enroll")
-    public @ResponseBody ResponseEntity<List<Enroll>> enrollSubjects(@RequestBody List<Enroll> enrolls){
-        return new ResponseEntity<>(enrollService.enroll(enrolls), HttpStatus.OK);
+    public ResponseEntity<Enroll> enrollSubjects(@RequestBody Enroll enroll){
+        return new ResponseEntity<>(enrollService.enroll(enroll), HttpStatus.OK);
     } 
 
     @GetMapping("/enrolled/{studentId}")
