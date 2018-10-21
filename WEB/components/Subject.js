a22de41de747e81aa60e19011d09b94e3b8d30e3
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 18:51:37 +0700

    [UPDATE] Web / remove console.log(), change localhost to Cloud Foundry url

--- WEB/components/Subject.js
+++ WEB/components/Subject.js
@@ -10,21 +10,17 @@ class Subject extends React.Component {
     }
 
     async enrollSubject(subjectId) {
-        console.log('====================================');
-        console.log(this.props.id + '   ' + subjectId);
-        console.log('====================================');
         const { data } = await axios.post('/enroll', {
             studentId: this.props.id,
             subjectId: subjectId,
             year: this.props.year
 
         }, {
-                baseURL: 'http://localhost:8080'
-            })
-        console.log('finish enroll')
-        console.log('====================================');
-        console.log({ data });
-        console.log('====================================');
+            baseURL: 'https://enrollservice-timely-emu.mybluemix.net'
+        })
+        if({data} && {data} != ''){
+            this.props.setMessage('Enroll Finish!!')
+        }
     }
 
     render() {
@@ -32,7 +28,6 @@ class Subject extends React.Component {
         return (
             <div className="row">
                 <div className="col-md-3"></div>
-                {console.log(this.props.data.id)}
                 <div className="col-md-2"><h5>{this.props.data.courseCode}</h5></div>
                 <div className="col-md-2"><h5>{this.props.data.courseName}</h5></div>
                 <div className="col-md-2"><h5>{this.props.data.lecturer}</h5></div>
