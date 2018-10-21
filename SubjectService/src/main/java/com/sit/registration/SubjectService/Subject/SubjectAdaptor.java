232f23ba95798a9572ecbbee203905505dbdf898
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 18:47:56 +0700

    [UPDATE] Subject / change local host to use Cloud Foundry url

--- SubjectService/src/main/java/com/sit/registration/SubjectService/Subject/SubjectAdaptor.java
+++ SubjectService/src/main/java/com/sit/registration/SubjectService/Subject/SubjectAdaptor.java
@@ -8,21 +8,21 @@ public class SubjectAdaptor {
 
     public Long[] getEnrolledSubjectId(long studentId){
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8080/enrolled/"+studentId;
+        String url = "https://enrollservice-timely-emu.mybluemix.net/enrolled/"+studentId;
         Long[] enrolledSubjectId = restTemplate.getForObject(url, Long[].class);
         return enrolledSubjectId;
     }
 
     public int getYear(long studentId){
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8100/user/"+studentId+"/year";
+        String url = "https://userservice-forgiving-nyala.mybluemix.net/user/"+studentId+"/year";
         int year = restTemplate.getForObject(url, Integer.class);
         return year;
     }
 
 	public int getUnAvailableSeat(long subjectId) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8080/seat/"+subjectId+"/unavailable";
+        String url = "https://enrollservice-timely-emu.mybluemix.net/seat/"+subjectId+"/unavailable";
         int seat = restTemplate.getForObject(url, Integer.class);
 		return seat;
 	}
