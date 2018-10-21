0b1bf00f1aa00c541c9f896e8140d2da3152e76d
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-21 17:35:29 +0700

    [UPDATE] User / user can login

--- UserService/src/main/java/com/sit/registration/UserService/User/UserController.java
+++ UserService/src/main/java/com/sit/registration/UserService/User/UserController.java
@@ -4,11 +4,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="*", maxAge=3600)
 public class UserController {
 
     @Autowired
@@ -22,6 +26,11 @@ public class UserController {
     @GetMapping("/user/{studentId}/year")
     public ResponseEntity<Long> getYearFromSpecificStudent(@PathVariable long studentId){
         return new ResponseEntity<>(userService.getYear(studentId), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> authentication(@RequestBody User user){
+        return new ResponseEntity<>(userService.login(user), HttpStatus.OK);
     }
     
 
